0e6eb388b1f15d9fe57997d8662ad9ca5b2aee6d
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-08-26 22:02:40 +0200

    fixed product thumbnail size

--- global_files/js/responsive-image.js
+++ global_files/js/responsive-image.js
@@ -42,20 +42,19 @@ responsiveImages.map(function(container) {
         case 'product-thumbnail':
             renderImage(container, publicID, alt, imgClass, {
                 mobile: [
+                    [200, 200],
+                    [300, 300],
+                    [400, 400],
                     [500, 500],
                     [600, 500],
                     [700, 700],
                     [800, 800],
-                    [900, 900],
-                    [1000, 1000],
-                    [1100, 1100],
-                    [1200, 1200]
                 ],
                 desktop: [
                     [300, 300],
                     [400, 400],
                     [500, 500],
-                    [600, 600],
+                    [600, 600]
                 ]
             });
             break;
