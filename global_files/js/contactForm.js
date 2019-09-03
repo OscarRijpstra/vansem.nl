567601049ef47c3a818dfd270d4e471c84ae78b0
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-09-03 22:29:00 +0200

    updated form js

--- global_files/js/contactForm.js
+++ global_files/js/contactForm.js
@@ -1,14 +1,48 @@
 const form = document.getElementsByClassName('form')[0];
-const recaptchaInput = form.getElementsByClassName('recaptcha__input')[0];
 const recaptchaKey = form.getAttribute('recaptchakey');
+const baseUrl = form.getAttribute('data-baseUrl');
+const errorMessage = document.getElementsByClassName('error__message')[0];
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
+    submitForm();  
+})
+
+const recaptchaResponse = () => new Promise((resolve, reject) => {
     grecaptcha.ready(function() {
-        grecaptcha.execute(recaptchaKey, {action: 'contact'}).then(function(token) {
-            recaptchaInput.value = token;
-            form.submit();
-        });
+        grecaptcha.execute(recaptchaKey, {action: 'contact'})
+        .then(function(token){ resolve(token) })
     });
 })
+
+async function submitForm() {
+    const recaptcha_response = await recaptchaResponse();
+
+    const fullname = form.querySelector('[name="fullname"]').value;
+    const phone = form.querySelector('[name="phone"]').value;
+    const email = form.querySelector('[name="email"]').value;
+    const question = form.querySelector('[name="question"]').value;
+
+    const resp = await fetch(`https://sdowerkt.herokuapp.com/vansem.nl/contact?page=${baseUrl}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            fullname,
+            phone,
+            email,
+            question,
+            recaptcha_response
+        })
+    })
+    
+    const message = await resp.json();
+
+    if (message.status == 'ok') return window.location.replace("/contact-succes");
+
+    if (message.status == 'invalid') {
+        errorMessage.innerText = `Foutmelding: ${message.error}`;
+    }
+}
