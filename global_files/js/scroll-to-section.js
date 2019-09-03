f34fc098c3ff4105eaeb1a178bf4f0b4a2976c9a
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-09-03 11:51:05 +0200

    fixed dynamic content on verhuur

--- global_files/js/scroll-to-section.js
+++ global_files/js/scroll-to-section.js
@@ -4,16 +4,24 @@ const scrollTriggers = Array.from(document.getElementsByClassName('trigger-scrol
 
 scrollTriggers.map(trigger => {
     trigger.addEventListener('click', () => {
+        changeNavigationSelector(trigger.getAttribute('data-section'));
         scrollToSection(trigger.getAttribute('data-section'));
     })
 })
 
-function scrollToSection(section) {
+function scrollToSection(section, offset) {
     const element = document.getElementById(section);
+    console.log(Number(offset))
     element.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
-        inline: 'start',
-        offsetTop: -100
+        inline: 'start'
+    })
+}
+
+function changeNavigationSelector(section) {
+    Array.from(document.getElementsByClassName('navigation__item-desktop')).map(navItem => {
+        if (navItem.getAttribute('data-section') == section) navItem.classList.add('navigation__item-current')
+        else navItem.classList.remove('navigation__item-current')
     })
 }
