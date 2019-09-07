49b046447ef5259c3892dfef1f444643286af68f
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-09-07 10:01:42 +0200

    added new image type

--- global_files/js/responsive-image.js
+++ global_files/js/responsive-image.js
@@ -1,15 +1,15 @@
 const responsiveImages = Array.from(document.getElementsByClassName('responsive__img'));
-const imgExtentions = ['webp', 'wdp' ,'jp2', 'jpg'];
+const imgExtentions = ['webp', 'wdp', 'jp2', 'jpg'];
 const imageHost = 'https://res.cloudinary.com/dyubvu51u/image/upload/';
-const cloudName =  'v1566569858';
+const cloudName = 'v1566569858';
 const folder = 'Vansem';
 
-responsiveImages.map(function(container) {
+responsiveImages.map(function (container) {
     const publicID = container.getAttribute('data-public-id');
     const type = container.getAttribute('data-type');
     const alt = container.getAttribute('data-alt') || '';
     const imgClass = container.getAttribute('data-class') || '';
-    
+
     switch (type) {
         case 'header':
             renderImage(container, publicID, alt, imgClass, {
@@ -58,6 +58,25 @@ responsiveImages.map(function(container) {
                 ]
             });
             break;
+
+        case 'imageContainer-item':
+            renderImage(container, publicID, alt, imgClass, {
+                mobile: [
+                    [400, 400],
+                    [500, 500],
+                    [600, 600],
+                    [700, 700],
+                    [800, 800],
+                    [900, 900],
+                    [1000, 1000],
+                    [1100, 1100]
+                ],
+                desktop: [
+                    [300, 300],
+                    [400, 400]
+                ]
+            });
+            break;
     }
 })
 
@@ -65,14 +84,14 @@ function renderImage(container, publicID, alt, imgClass, resolutions) {
     const picture = document.createElement('picture');
     container.appendChild(picture);
 
-    imgExtentions.map(function(extention) {
+    imgExtentions.map(function (extention) {
         if (resolutions.mobile.length) {
             const source = document.createElement('source');
             picture.appendChild(source);
             source.media = '(max-width: 930px)';
             source.type = 'image/' + extention;
 
-            const sourceValue = resolutions.mobile.map(function(resolution) {
+            const sourceValue = resolutions.mobile.map(function (resolution) {
                 return imageHost + 'q_50,c_fill,w_' + resolution[0] + (resolution[1] != 'auto' ? ',h_' + resolution[1] : '') + '/' + cloudName + '/' + folder + '/' + publicID + '.' + extention + ' ' + resolution[0] + 'w';
             });
             source.srcset = sourceValue.join(', ');
@@ -84,7 +103,7 @@ function renderImage(container, publicID, alt, imgClass, resolutions) {
             source.media = '(min-width: 930px)';
             source.type = 'image/' + extention;
 
-            const sourceValue = resolutions.desktop.map(function(resolution) {
+            const sourceValue = resolutions.desktop.map(function (resolution) {
                 return imageHost + 'q_50,c_fill,w_' + resolution[0] + (resolution[1] != 'auto' ? ',h_' + resolution[1] : '') + '/' + cloudName + '/' + folder + '/' + publicID + '.' + extention + ' ' + resolution[0] + 'w';
             });
             source.srcset = sourceValue.join(', ');
