57c0c13e0cee2e75536e3ef8c8c0ce10a1b46aa8
Rijpstra, Oscar - Student Rijn IJssel <00276969@rijnijssel.nl>
2019-10-22 20:40:17 +0200

    added js events

--- global_files/js/index.js
+++ global_files/js/index.js
@@ -1,9 +1,23 @@
 const navigationToggle = Array.from(document.getElementsByClassName('toggle__navigation-items'));
 const navigationItems = Array.from(document.getElementsByClassName('navigation__items'));
+const phoneClickEvent = Array.from(document.getElementsByClassName('google-phone-event'));
+const emailClickEvent = Array.from(document.getElementsByClassName('google-email-event'));
 
 navigationToggle.map(function(toggle){
     toggle.addEventListener('click', toggleNavigationItems);
 })
+
+phoneClickEvent.map(phone => phone.addEventListener('click', () => {
+    gtag('event', 'Klik', {
+        'event_category': 'Telefoon'
+    });
+}));
+
+emailClickEvent.map(email => email.addEventListener('click', () => {
+    gtag('event', 'Klik', {
+        'event_category': 'Email'
+    });
+}));
 
 function toggleNavigationItems() {
     navigationItems.map(function(naviation){
